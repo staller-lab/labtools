47b9dde3d154e15d52fa2c9e763c59a84f673e95
MassiveJords <jastefani@ucdavis.edu>
2023-02-21 15:21:31 -0800

    removing code

--- src/labtools/adtools/_alternative/datareader.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_ADs(ADs):
-    """Take a set of AD objects and return the sequences."""
-
-    for AD in ADs:
-        print(AD.count, AD.seq)
-        for code in AD.barcodes:
-            print(code.seq, code.count)
